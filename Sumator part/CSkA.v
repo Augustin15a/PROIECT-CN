12e0c265633007c8b9c5d71787daa9ea98829975
Augustin15a <92544752+Augustin15a@users.noreply.github.com>
2026-04-23 15:42:40 +0000

    implementare scadere

--- Sumator part/CSkA.v
+++ Sumator part/CSkA.v
@@ -10,18 +10,25 @@ module CSkA(
 
 wire [3:0]carry;
 wire [2:0]carryOut;
+wire [8:0]y_in;
+
+genvar i,j;
+generate
+    for(j = 0; j < 9; j = j + 1) begin : xor_gen
+        xor(y_in[j], y[j], sum);
+    end
+endgenerate
 
 buf(carry[0],sum);
 
 generate
-	genvar i;
 	for(i = 0;i < 3; i = i + 1) 
 		begin : rca_gen
 			wire carryCalc;//folosit pentru calculul lui c[i+1]
 			wire propagate;
 			RCA3 rca3(
 				.x(x[3*i + 2 : 3*i]),
-				.y(y[3*i + 2 : 3*i]),
+				.y(y_in[3*i + 2 : 3*i]),
 				.carryIn(carry[i]),
 				.carryOut(carryOut[i]),
 				.p(propagate),
