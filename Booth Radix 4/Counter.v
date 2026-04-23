fd17afdf12f8601655f92ebaf598741aaacaaa7a
Augustin15a <92544752+Augustin15a@users.noreply.github.com>
2026-04-23 21:12:15 +0000

    conter 2.1v

--- Booth Radix 4/Counter.v
+++ Booth Radix 4/Counter.v
@@ -1,39 +1,54 @@
 `include "FF_D.v"
 `include "FACWP.v"
-
-module Counter(
+module Counter #(parameter max)(
     input  clk,
     input  rst,
     input  enable,
-    output [1:0] q,
+    output [max:0] q,
     output done
 );
 
-wire [1:0] d;
-wire carry0;
-wire p0, p1;
+wire [max:0]d;
+wire [max:0]c;
+wire [max:0]p;
 
-FACWP fa0(
-    .x       (q[0]),
-    .y       (enable),
-    .carryIn (1'b0),
-    .z       (d[0]),
-    .carryOut(carry0),
-    .p       (p0)
-);
+genvar i,j,k;
+FACWP fa0(.x(q[0]), .y(enable), .carryIn(1'b0), .z(d[0]), .carryOut(c[0]), .p(p[0]));
+generate
+	for(i = 1;i < max;i = i + 1)
+	begin facgen:
+		FACWP fac(.x(q[i]), .y(1'b0),   .carryIn(c[i-1]),   .z(d[i]), .carryOut(c[i]), .p(p[i]));
+	end
+endgenerate
 
-FACWP fa1(
-    .x       (q[1]),
-    .y       (1'b0),
-    .carryIn (carry0),
-    .z       (d[1]),
-    .carryOut(),
-    .p       (p1)
-);
+generate
+	for(j = 0;j < max;j = j + 1)
+	begin ffgen:
+		FF_D ff0(.clk(clk), .rst(rst), .d(d[j]), .enable(1'b1), .q(q[j]));
+	end
+endgenerate
 
-FF_D ff0(.clk(clk), .rst(rst), .d(d[0]), .enable(1'b1), .q(q[0]));
-FF_D ff1(.clk(clk), .rst(rst), .d(d[1]), .enable(1'b1), .q(q[1]));
+wire [max-1:0]not_q;
+generate
+	for(k = 0;k < max;k = k + 1)
+	begin notgen:
+		not(not_q[k], q[k]);
+	end
+endgenerate
 
-and(done, q[1], q[0]);
+generate
+    if (max == 3)
+	begin : facgen_3
+		wire done_3;
+		and(done_3, q[1], q[0]);	
+        buf(done, done_3);
+	end
+    else
+	begin : facgen_16
+		wire done_16;
+		and(done_16, q[4], not_q[3], not_q[2], not_q[1], not_q[0]);
+        buf(done, done_16);
+	end
+endgenerate
 
 endmodule
